15622991615499240c9e13b5c6fe8e44bea57270
detomon <simon@monoxid.ch>
2017-03-12 20:04:25 +0100

    Rewrite code

--- autocreate.js
+++ autocreate.js
@@ -37,19 +37,14 @@
 	'use strict';
 
 	/**
-	 * Value objects for data attributes.
-	 */
-	var dataValues = {};
-
-	/**
-	 * Incrementing key index to ensure uniqueness of element key.
-	 */
-	var keyIndex = 0;
-
-	/**
 	 * Random data attribute name.
 	 */
-	var key = 'ac' + rand();
+	var key = 'ac' + randomString();
+
+	/**
+	 * Data attribute name.
+	 */
+	var dataKey = 'data-' + key;
 
 	/**
 	 * The topmost DOM element.
@@ -72,23 +67,36 @@
 	var removedTimeout;
 
 	/**
+	 * Element prototype.
+	 */
+	var elementPrototype = Element.prototype;
+
+	/**
+	 * Element matches method.
+	 */
+	var elementMatches = elementPrototype.matches
+			|| elementPrototype.matchesSelector
+			|| elementPrototype.msMatchesSelector
+			|| elementPrototype.mozMatchesSelector
+			|| elementPrototype.webkitMatchesSelector
+			|| elementPrototype.oMatchesSelector;
+
+	/**
 	 * Create random string.
 	 *
 	 * @return String
 	 */
-	function rand() {
-		var r = Math.random;
-
-		return ((((1 + r()) * 0x100000)) | 0) + '' + ((((1 + r()) * 0x100000)) | 0);
+	function randomString() {
+		return Math.random().toString(36).substring(2, 12);
 	}
 
 	/**
-	 * Make list unique by removing duplicate items.
+	 * Make unique list by removing duplicate items.
 	 *
 	 * @param Array array A list of arbitary items.
 	 * @return Array A list with unique elements.
 	 */
-	function unique(array) {
+	function arrayUnique(array) {
 		return array.sort().reduce(function (list, value) {
 			if (list[list.length - 1] !== value) {
 				list.push(value);
@@ -98,44 +106,6 @@
 		}, []);
 	}
 
-	/**
-	 * Get or set data object fo given element.
-	 *
-	 * @param Node element A DOM node.
-	 * @param Object value A value object.
-	 * @return Object The value object.
-	 */
-	function data(element, value) {
-		if (element && !element.dataset) {
-			return false;
-		}
-		if (value === undefined) {
-			value = dataValues[element.dataset[key]];
-		}
-		else {
-			var index = element.dataset[key];
-
-			if (index === undefined) {
-				index = element.dataset[key] = rand() + (keyIndex ++);
-				keyIndex %= 0x100000000;
-			}
-
-			value._id = rand();
-			dataValues[index] = value;
-		}
-
-		return value;
-	}
-
-	/**
-	 * Unset data value for given element in global list.
-	 *
-	 * @param Node element A DOM node.
-	 */
-	function unsetData(element) {
-		delete dataValues[element.dataset[key]];
-	}
-
  	/**
  	 * Loop over given NodeList.
 	 *
@@ -143,196 +113,13 @@
 	 * @param Function func A callback function.
  	 */
 	function forEachNode(nodes, func) {
-		for (var i = 0; i < nodes.length; i ++) {
+		for (var i = 0; i < nodes.length; i++) {
 			var node = nodes[i];
 
-			if (node.nodeType === Node.ELEMENT_NODE) {
+			if (node.nodeType === Element.ELEMENT_NODE) {
 				func(node);
 			}
 		}
-	}
-
-	/**
-	 * Get observer context object for given node.
-	 *
-	 * @param Node A DOM node.
-	 * @return Object The context object.
-	 */
-	function observerCtx(parent) {
-		var ctx = parent[key];
-
-		if (!ctx) {
-			var observer = new MutationObserver(function (mutations) {
-				mutations.forEach(function (mutation) {
-					forEachNode(mutation.addedNodes, function (node) {
-						handleCtx(ctx, parent, node);
-					});
-				});
-			});
-
-			ctx = parent[key] = {
-				modules: {},
-				observer: observer,
-			};
-
-			observer.observe(parent, {childList: true, subtree: true});
-		}
-
-		return ctx;
-	}
-
-	/**
-	 * Get element context object for given node.
-	 *
-	 * @param Node A DOME node.
-	 * @return Object The context object.
-	 */
-	function elementCtx(element) {
-		var ctx = data(element);
-
-		if (!ctx) {
-			ctx = data(element, {
-				data: {},
-			});
-		}
-
-		return ctx;
-	}
-
-	/**
-	 * Call different versions of Node's matches method.
-	 *
-	 * @param Node A DOM node.
-	 * @param String The selector to match.
-	 * @return bool
-	 */
-	function matches(element, selector) {
-		var m = element.matches
-			|| element.matchesSelector
-			|| element.msMatchesSelector
-			|| element.mozMatchesSelector
-			|| element.webkitMatchesSelector
-			|| element.oMatchesSelector
-			|| function () {
-				return [].indexOf.call(document.querySelectorAll(selector), element) !== -1;
-			};
-
-		return m.call(element, selector);
-	}
-
-	/**
-	 * Handle module instance for given element.
-	 *
-	 * @param AutoCreate modules The module instance to create.
-	 * @param Node parent The parent node for the created node.
-	 * @param Node target The created element.
-	 */
-	function handleModule(module, parent, target) {
-		var allElements = [];
-
-		if (target) {
-			if (matches(target, module.selector)) {
-				allElements.push(target);
-			}
-
-			forEachNode(target.querySelectorAll(module.selector), function (node) {
-				allElements.push(node);
-			});
-		}
-		else {
-			forEachNode(parent.querySelectorAll(module.selector), function (node) {
-				allElements.push(node);
-			});
-		}
-
-		allElements.forEach(function (element) {
-			var ctx = elementCtx(element);
-
-			if (!ctx.data[module.id]) {
-				var object = ctx.data[module.id] = {
-					_module: module,
-				};
-
-				module.elements[ctx._id] = element;
-				module.createCtx.call(object, element);
-			}
-		});
-	}
-
-	/**
-	 * Handle all module instances for given element.
-	 *
-	 * @param Object ctx The observer context.
-	 * @param Node parent The parent node for the created node.
-	 * @param Node target The created element.
-	 */
-	function handleCtx(ctx, parent, target) {
-		var modules = ctx.modules;
-
-		for (var i in modules) {
-			if (modules.hasOwnProperty(i)) {
-				var module = modules[i];
-
-				handleModule(module, parent, target);
-			}
-		}
-	}
-
-	/**
-	 * Destroy element with a given module.
-	 *
-	 * @param Node element A DOM element to destroy.
-	 * @param AutoCreate The modules to destroy the element for.
-	 * @param Object The node context.
-	 */
-	function destroyElementModule(element, module, ctx) {
-		var moduleData = module;
-		var module = moduleData._module;
-
-		module.destroyCtx.call(moduleData, element);
-		delete module.elements[ctx._id];
-	}
-
-	/**
-	 * Destroy single node.
-	 *
-	 * @param Node element A DOM element to destroy.
-	 * @param boolean delayed If the element should be removed immediately.
-	 */
-	function destroyElement(element, delayed) {
-		var ctx = data(element);
-
-		if (ctx) {
-			var success = false;
-			var modules = ctx.data;
-
-			for (var i in modules) {
-				if (modules.hasOwnProperty(i)) {
-					var module = modules[i];
-
-					if (module._module.destroyDelayed === delayed) {
-						destroyElementModule(element, module, ctx);
-						success = true;
-					}
-				}
-			}
-
-			if (success) {
-				unsetData(element);
-			}
-		}
-	}
-
-	/**
-	 * Remove nodes.
-	 *
-	 * @param Array elements A list of elements to remove.
-	 * @param boolean If the elements should be removed immediately.
-	 */
-	function destroyElements(elements, delayed) {
-		elements.forEach(function (element) {
-			destroyElement(element, delayed);
-		});
 	}
 
 	/**
@@ -343,72 +130,61 @@
 	}
 
 	/**
-	 * Remove nodes pushed to the delayed removing list.
 	 */
-	function removeDelayed() {
-		if (removedTimeout) {
-			clearTimeout(removedTimeout);
+	function elementContext(element) {
+		var ctx = element[key];
+
+		if (!ctx) {
+			ctx = element[key] = new Context(element);
 		}
 
-		removedTimeout = setTimeout(function () {
-			var elements = removedElements.filter(function (element) {
-				for (var parent = element; parent.parentNode;) {
-					parent = parent.parentNode;
-				}
-
-				return parent !== element.ownerDocument;
-			});
-
-			// may be filled again by destructors
-			removedElements = [];
-			destroyElements(elements, true);
-		}, 0);
-	}
+		return ctx;
+	};
 
 	/**
 	 * Initialize DOM observer.
 	 */
 	function init() {
+		if (domObserver) {
+			return;
+		}
+
 		domObserver = new MutationObserver(function (mutations) {
+			var dataSelector = '[' + dataKey + ']';
+
 			mutations.forEach(function (mutation) {
-				forEachNode(mutation.removedNodes, function (node) {
-					var elements = node.querySelectorAll('[data-' + key + ']');
+				forEachNode(mutation.removedNodes, function (element) {
+					var elements = element.querySelectorAll(dataSelector);
 
 					forEachNode(elements, function (element) {
-						var ctx = data(element);
+						var ctx = elementContext(element);
 
 						if (ctx) {
-							destroyElement(element, false);
-							removedElements.push(element);
+							ctx.destroy();
 						}
 					});
 
-					var ctx = data(node);
+					var ctx = elementContext(element);
 
 					if (ctx) {
-						destroyElement(node, false);
-						removedElements.push(node);
+						ctx.destroy();
 					}
 				});
 			});
 
-			removeDelayed();
+			// TODO: delayed
+
+			//removeDelayed();
 		});
 
 		domObserver.observe(dom, {childList: true, subtree: true});
 	}
 
 	/**
-	 * AutoCreate module.
-	 *
-	 * @param Object options:
-	 *   selector: Element selector (string).
-	 *   parents: Single element or array of parents to observe (optional).
-	 *   create: The function to be called when an element is created.
-	 *   destroy: The function to be called when the element is removed from the DOM (optional).
-	 *   destroyDelayed: Does not destroy elements moved to other parent elements (optional).
 	 */
-	function AutoCreate(options) {
+	var Module = function (options) {
+		options = options || {};
+
 		var selector = options.selector || error('Selector cannot be empty');
 		var parents = options.parents || dom;
 
@@ -417,78 +193,197 @@
 			parents = [parents];
 		}
 
-		parents = unique(parents);
+		parents = arrayUnique(parents);
 
-		this.id = rand();
-		this.parents = parents;
+		this.id = randomString();
 		this.selector = selector;
-		this.createCtx = options.create || function () {};
-		this.destroyCtx = options.destroy || function () {};
+		this.parents = parents;
+		this.createElement = options.create || function () {};
+		this.destroyElement = options.destroy;
 		this.destroyDelayed = !!options.destroyDelayed;
 		this.elements = {};
 
-		for (var i = 0; i < parents.length; i ++) {
-			var ctx = observerCtx(parents[i]);
-			ctx.modules[this.id] = this;
-			ctx.options = options;
-		}
-	}
-
-	/**
-	 * Get context form given node.
-	 *
-	 * @param Node element A DOM element.
-	 * @return Object The context.
-	 */
-	AutoCreate.prototype.ctxFromElement = function (element) {
-		var ctx = data(element);
-
-		if (ctx) {
-			return ctx.data[this.id];
-		}
-	};
-
-	/**
-	 * Destroy module instances and remove observers.
-	 */
-	AutoCreate.prototype.destroy = function () {
-		var parents = this.parents;
-		var elements = this.elements;
-
-		for (var i = 0; i < parents.length; i ++) {
-			var ctx = observerCtx(parents[i]);
-
-			for (var i in elements) {
-				if (elements.hasOwnProperty(i)) {
-					destroyElementModule(elements[i], this, ctx);
-				}
-			}
-
-			delete ctx.modules[this.id];
-		}
-	};
-
-	/**
-	 * Handle initial modules.
-	 */
-	AutoCreate.prototype.handleModules = function () {
 		this.parents.forEach(function (parent) {
-			handleModule(this, parent);
+			elementContext(parent).addModule(this);
+			this.createAll(parent);
 		}.bind(this));
 	}
 
 	/**
-	 * Create new autocreate module and return instance.
-	 *
-	 * @param Object options Options for the AutoCreate class.
-	 * @return AutoCreate The autocreate module instance.
+	 */
+	Module.prototype.create = function (element) {
+		var moduleId = this.id;
+		var ctx = elementContext(element);
+		var data = ctx.data;
+
+		if (!data[moduleId]) {
+			var object = data[moduleId] = {
+				module: this,
+				userCtx: {},
+			};
+
+			this.elements[ctx.id] = element;
+			this.createElement.call(object.userCtx, element);
+		}
+	};
+
+	/**
+	 */
+	Module.prototype.destroy = function () {
+		var moduleId = this.id;
+		var elements = this.elements;
+		var parents = this.parents;
+		var destroy = this.destroyElement;
+
+		for (var i in elements) {
+			if (elements.hasOwnProperty(i)) {
+				var element = elements[i];
+				var ctx = elementContext(element);
+
+				ctx.removeModule(this);
+			}
+		}
+
+		for (var i in parents) {
+			if (parents.hasOwnProperty(i)) {
+				var parent = parents[i];
+				var ctx = elementContext(parent);
+
+				ctx.removeModule(module);
+			}
+		}
+
+		this.elements = {};
+		this.parents = [];
+	};
+
+	/**
+	 */
+	Module.prototype.createAll = function (target) {
+		var self = this;
+		var selector = this.selector;
+
+		if (elementMatches.call(target, selector)) {
+			self.create(target);
+		}
+
+		forEachNode(target.querySelectorAll(selector), function (element) {
+			self.create(element);
+		});
+	};
+
+	/**
+	 */
+	var Context = function (element) {
+		this.id = randomString();
+		this.data = {};
+		this.element = element; // TODO: remove and use function argument?
+		element.setAttribute(dataKey, '');
+	};
+
+	/**
+	 */
+	Context.prototype.addObserver = function () {
+		if (!this.observer) {
+			var self = this;
+			var observer = new MutationObserver(function (mutations) {
+				mutations.forEach(function (mutation) {
+					forEachNode(mutation.addedNodes, function (element) {
+						self.createAll(element);
+					});
+				});
+			});
+
+			this.modules = {};
+			this.observer = observer;
+			observer.observe(this.element, {childList: true, subtree: true});
+		}
+	};
+
+	/**
+	 */
+	Context.prototype.removeObserver = function () {
+		if (this.observer) {
+			this.observer.disconnect();
+			delete this.modules;
+			delete this.observer;
+		}
+	};
+
+	/**
+	 */
+	Context.prototype.addModule = function (module) {
+		this.addObserver();
+		this.modules[module.id] = module;
+	};
+
+	/**
+	 */
+	Context.prototype.removeModule = function (module) {
+		// TODO: make faster?
+
+		var moduleId = module.id;
+		var data = this.data;
+		var modules = this.modules;
+
+		if (data[moduleId]) {
+			module.destroyElement.call(data[moduleId].userCtx, this.element);
+			delete data[moduleId];
+		}
+
+		var modulesLength = 0;
+
+		if (modules) {
+			delete modules[moduleId];
+			modulesLength = Object.keys(modules).length;
+		}
+
+		if (!modulesLength) {
+			this.removeObserver();
+
+			if (!Object.keys(data).length) {
+				this.destroy();
+			}
+		}
+	};
+
+	/**
+	 */
+	Context.prototype.createAll = function (target) {
+		var modules = this.modules;
+
+		for (var i in modules) {
+			if (modules.hasOwnProperty(i)) {
+				modules[i].createAll(target);
+			}
+		}
+	};
+
+	/**
+	 */
+	Context.prototype.destroy = function () {
+		var element = this.element;
+		var data = this.data;
+
+		// before element destruction call to prevent possible recursion
+		this.removeObserver();
+
+		for (var i in data) {
+			if (data.hasOwnProperty(i)) {
+				module.destroyElement.call(data[i].userCtx, element);
+			}
+		}
+
+		element.removeAttribute(dataKey);
+		delete element[key];
+	};
+
+	/**
 	 */
 	function autocreate(options) {
-		var instance = new AutoCreate(options);
+		init();
 
-		instance.handleModules();
-
-		return instance;
+		return new Module(options);
 	}
 
 	var $ = window.jQuery || window.ujs;
@@ -502,8 +397,6 @@
 			return autocreate(options);
 		};
 	}
-
-	init();
 
 	return autocreate;
 }));
